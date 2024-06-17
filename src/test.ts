45f237db08f0625d1e453d4e47971a86c9176c47
Mohd Shahenvaz Khan <126674257+shahenvaz-khan-knoldus@users.noreply.github.com>
2024-06-17 17:56:56 +0530

    made some changes on src directory for pipeline trigger

--- src/test.ts
+++ src/test.ts
@@ -20,7 +20,7 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting(),
 );
 
-// Then we find all the tests.
+// Then we find all the tests. adding this comment for pipeline testing
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().forEach(context);
