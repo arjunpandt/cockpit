e732eca077ca945a3fce89b5610b9c42494b1c21
Mohd Shahenvaz Khan <126674257+shahenvaz-khan-knoldus@users.noreply.github.com>
2024-06-20 14:47:33 +0530

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -20,7 +20,7 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting(),
 );
 
-// Then we find all the tests. adding this comment for pipeline testing 11
+// Then we find all the tests. adding this comment for pipeline testing 12
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().forEach(context);
